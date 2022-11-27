5c8afdd6525befa923fa19c43a00a5a5aea501b8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-27 16:56:46 -0400

    a

---
+++ fundamentos/tiposBasicos.py
@@ -0,0 +1,11 @@
+isTrue = False
+age = 26
+name = "weslley"
+weight = 73.5
+listOfNumber = [1,2,3,4,5,6]
+
+print(f"Seu nome é {name} e sua idade é {age} e seu peso é {weight}")
+print(f"Vai dar tudo certo {isTrue}")
+
+for number in listOfNumber:
+    print(f"O numero é {number}\n")
