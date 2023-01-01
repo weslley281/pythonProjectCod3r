caac7fbe70f0a5a2879cc0c4a82219e625c24cc4
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-01 13:08:35 -0400

    a

---
+++ fundamentos/builtins.py
@@ -0,0 +1,9 @@
+print(type(1))
+print(type("A"))
+print(type(True))
+print(type([1,2,3]))
+print(type(("A","B")))
+print(type({"a": 2, "b": 4}))
+
+a = 7
+print(dir)
---
+++ fundamentos/maisOperadores.py
@@ -0,0 +1,15 @@
+lista = [1, 2 , 3, 'Fulano', 'Cicrano']
+
+print(2 in lista)
+print('Deltrano' in lista)
+
+x = 3
+y = x
+z = 3
+print(x is y)
+print(y is z)
+print(x is not z)
+
+lista2 = lista
+
+print(lista2 is lista2)
