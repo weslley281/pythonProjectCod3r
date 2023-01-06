c751c8b8cc1b6def741ec3d9dcad141290a76d2a
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-06 06:49:05 -0400

    a

---
+++ fundamentos/string.py
@@ -0,0 +1,9 @@
+print(dir(str))
+
+nome = "Weslley"
+print(f"{nome[0]} -- {nome[6]}")
+
+print("aaaaaaaaaaaaaaaaaa\n"
+      "aaaaaaaaaaaaaaaa\n"
+      "aaaaaaaaaaaaaaa")
+
---
+++ fundamentos/string2.py
@@ -0,0 +1,11 @@
+nome = "Weslley"
+
+print(f"{nome[-2]} e {nome[0]}")
+print(nome[:4])
+print(nome[4:])
+
+numeros = "0123456789"
+print(numeros)
+print(numeros[::])
+print(numeros[::2])
+print(numeros[1::2])
