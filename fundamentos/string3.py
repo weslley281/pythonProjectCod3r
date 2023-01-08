99166dc5988bae04387be4bc26490c9f663bbebf
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-08 16:01:30 -0400

    a

---
+++ fundamentos/string3.py
@@ -0,0 +1,10 @@
+frase = "Alguma coisa esta acontecendo"
+print('coisa' in frase)
+print('sei la' not in frase)
+print(len(frase))
+print(frase)
+print(frase.upper())
+print(frase)
+
+matriz = frase.split(' ')
+print(matriz[0])
---
+++ fundamentos/string4.py
@@ -0,0 +1,6 @@
+a = '123\n'
+b = "Alguma Coisa\n"
+
+print(a.__add__(b))
+print(len(a))
+print(a.__len__())
