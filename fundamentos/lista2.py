4c17d78aa9371ea6a2b1e08ed1f6a93a6a3b46ea
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-10 06:27:11 -0400

    a

---
+++ fundamentos/lista2.py
@@ -0,0 +1,7 @@
+lista = [1, 2, 3, "Fulano", "Cicrano", "Deltrano", 3.14]
+print(lista.index("Fulano"))
+print(lista[4])
+print("Deltrano" in lista)
+print("MAltrono" not in lista)
+print(lista[-5])
+
---
+++ fundamentos/lista3.py
@@ -0,0 +1,8 @@
+lista = ["Fulano", "Cicrano", "Deltrano", "Matrano", "Beltrana"]
+print(lista[1:3])
+print(lista[1:-1])
+print(lista[1:])
+print(lista[::2])
+
+del lista[2]
+print(lista)
