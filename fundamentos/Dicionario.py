c26ba404f0cf174cc60aee0328fc36c3e34105f6
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-02-11 08:40:18 -0300

    a

---
+++ fundamentos/Dicionario.py
@@ -0,0 +1,8 @@
+pessoa = {'nome': "Weslley", 'idade': 27, 'cursos': ['python', 'javascript', 'typescript', 'java', 'php']}
+print(pessoa)
+print(type(pessoa))
+print(dir(pessoa))
+print(len(pessoa))
+print(pessoa['nome'])
+print(pessoa.keys())
+print(pessoa.items())
---
+++ fundamentos/tupla.py
@@ -0,0 +1,14 @@
+tupla = tuple()
+tupla = ()
+print(type(tupla))
+print(dir(tupla))
+
+tupla = ('um',)
+print(type(tupla))
+
+# tupla(0) = 'novo'
+cores = ('green', 'yellow', 'blue', 'white')
+print(cores[0])
+print(cores[-1])
+print(cores[1:])
+print(len(cores))
