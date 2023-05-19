279153c8817ecc3a4a4924c0fb133bfbb6370bed
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-05-19 11:41:54 -0400

    a

--- funcoes/gerador_html_v1.py
+++ funcoes/gerador_html_v1.py
@@ -2,7 +2,7 @@ def tag_bloco(texto, classe="success"):
     return f'<div class="{classe}">{texto}</div>'
 
 if __name__ == '__main__':
-    assert tag_bloco('Incluido com sucesso!') == \
-        '<div class="success">Incluido com sucesso!<div>'
+    assert tag_bloco('Incluido com sucesso!') == '<div class="success">Incluido com sucesso!</div>'
 
 
+print(tag_bloco("aaaaaaaaaaa"))
