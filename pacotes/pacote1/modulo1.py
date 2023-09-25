7f4b65fa6650a5861760d51a1a0cf1a6a4d39b81
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-09-25 06:20:25 -0400

    a

---
+++ pacotes/pacote1/modulo1.py
@@ -0,0 +1,4 @@
+print('importado: ' + __name__ + ' ' + __package__)
+
+def soma(n1, n2):
+    return n1 + n2;
---
+++ pacotes/pacote1/modulo2.py
@@ -0,0 +1,6 @@
+def main():
+    print("Rodando o main()")
+
+
+if __name__ == '__main__':
+    main()
