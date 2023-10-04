9b1a188e84f3cfaca74cca5392f911a1ca4b58e7
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-10-04 06:35:21 -0400

    a

---
+++ poo_avançado/evolucao_v1.py
@@ -0,0 +1,16 @@
+class Humano:
+    especie = "Doido"
+    def __init__(self, nome: str):
+        self.nome = nome
+
+    def do_outro_time(self):
+        self.especie = "Do Outro Time"
+        return self
+
+if __name__ == '__main__':
+    fulano = Humano("Fulano")
+    cicrano = Humano("Cicrano").do_outro_time()
+
+    print(f'Especie do humano: {Humano.especie}')
+    print(f'Especie do fulano: {fulano.especie}')
+    print(f'Especie do cicrano: {cicrano.especie}')
